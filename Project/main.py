704f02c234d906eb47327b65a4e051fda2f2178e
boanca-tudor <tudorboanca@gmail.com>
2023-05-16 12:14:45 +0300

    Inception score now uses custom age classifier, image is normalized in (-1, 1) interval

--- Project/main.py
+++ Project/main.py
@@ -1,7 +1,7 @@
 from models.caae import *
 from model_utils import *
 import numpy as np
-import matplotlib.pyplot as plt
+from skimage.io import imsave
 
 
 if __name__ == '__main__':
@@ -10,20 +10,31 @@ if __name__ == '__main__':
                  l_channels=10,
                  gen_channels=1024,
                  dataset_size=len(list_full_paths(dataset_path)))
-    # training
-    # model.train(50, dataset_path, 64, '2023-04-24/25_epochs_UTKFace/')
+    # model.train(epochs=150, dataset_path=dataset_path,
+    #             batch_size=64,
+    #             save="every5",
+    #             previous_path='2023-05-14/50_epochs_UTKFace/')
 
     # eval
-    checkpoint_dir = '2023-04-24/25_epochs_UTKFace/'
+    checkpoint_dir = '2023-05-15/200_epochs_UTKFace/'
     model.load_model(checkpoint_dir)
 
     ages = create_all_ages()
     image = load_image('test.jpg')
-    results = [image]
-    image = tf.expand_dims(image, axis=0)
-    for age in ages:
-        results.append(tf.squeeze(model.eval([image, age])).numpy())
+    image2 = load_image('test4.jpg')
+    images = [image, image2]
+    results = []
+    all_results = []
+    for image in images:
+        results = [image]
+        for age in ages:
+            image_tensor = tf.expand_dims(image, axis=0)
+            results.append(tf.squeeze(model.eval([image_tensor, age])).numpy())
 
-    all_images = np.concatenate(results)
-    plt.imshow(all_images)
-    plt.show()
+        all_results.append(np.concatenate(results))
+
+    all_results = np.asarray(all_results)
+    final_result = all_results[0]
+    for i in range(1, len(all_results)):
+        final_result = np.concatenate((final_result, all_results[i]), axis=1)
+    imsave("result.jpg", (((final_result + 1) / 2) * 255).astype(np.uint8))
--- Project/models/metrics/inception_score.py
+++ Project/models/metrics/inception_score.py
@@ -4,9 +4,11 @@ from numpy import log
 from numpy import mean
 from numpy import std
 from numpy import exp
-from keras.applications.inception_v3 import InceptionV3
-from keras.applications.inception_v3 import preprocess_input
-from skimage.transform import resize
+from keras.applications.efficientnet_v2 import preprocess_input
+
+from models.caae import CAAE
+from models.age_classifier import *
+import cv2
 
 
 def scale_images(images, new_shape):
@@ -15,22 +17,20 @@ def scale_images(images, new_shape):
         new_image = resize(image, new_shape, 0)
         images_list.append(new_image)
 
-    return images_list
+    return np.asarray(images_list)
 
 
 def calculate_inception_score(images, n_split=10, eps=1E-16):
-    model = InceptionV3()
+    inception_model = create_model(128)
+    inception_model.load_weights("../../age_classifier_checkpoints/weights.08.hdf5")
     scores = list()
     n_part = floor(images.shape[0] / n_split)
     for i in range(n_split):
         ix_start, ix_end = i * n_part, (i + 1) * n_part
         subset = images[ix_start: ix_end]
 
-        subset = subset.astype('float32')
-        subset = scale_images(subset, (299, 299, 3))
         subset = preprocess_input(subset)
-
-        p_yx = model.predict(subset)
+        p_yx = inception_model.predict(subset)
 
         p_y = expand_dims(p_yx.mean(axis=0), 0)
         kl_d = p_yx * (log(p_yx + eps) - log(p_y + eps))
@@ -40,4 +40,31 @@ def calculate_inception_score(images, n_split=10, eps=1E-16):
         scores.append(is_score)
 
     return mean(scores), std(scores)
+
+
+if __name__ == "__main__":
+    dataset_path = '../../data/UTKFace/'
+    model = CAAE(z_channels=100,
+                 l_channels=10,
+                 gen_channels=1024,
+                 dataset_size=len(list_full_paths(dataset_path)))
+
+    checkpoint_dir = '../../2023-05-15/200_epochs_UTKFace/'
+    model.load_model(checkpoint_dir)
+
+    ages = create_all_ages()
+    image_paths = list_full_paths(dataset_path)
+    np.random.shuffle(image_paths)
+    image_filenames = image_paths[0:500]
+    images = []
+    for image in image_filenames:
+        images.append(tf.expand_dims(load_image(image), axis=0))
+    results = []
+
+    for image in images:
+        for age in ages:
+            results.append(tf.squeeze(model.eval([image, age])).numpy())
+
+    is_avg, is_std = calculate_inception_score(np.asarray(results), n_split=10)
+    print(is_avg, is_std)
 
