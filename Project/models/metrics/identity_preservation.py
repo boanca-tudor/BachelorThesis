5adfbba64b7ac3613f98ce093d3c7be120ead191
boanca-tudor <tudorboanca@gmail.com>
2023-06-10 15:18:38 +0300

    Identity preservation computing for multiple images

--- Project/models/metrics/identity_preservation.py
+++ Project/models/metrics/identity_preservation.py
@@ -1,12 +1,11 @@
-import json
-
 import tensorflow as tf
+from facepplib.exceptions import ResourceAttrError
 from skimage.io import imsave
 import numpy as np
 
 from facepp_utils import *
 from models.caae import CAAE
-from models.model_utils import load_image
+from models.model_utils import *
 
 
 def load_model(model_path):
@@ -27,22 +26,44 @@ def generate_image(model, base_image_url, age, target_filepath):
     imsave(target_filepath, (((result + 1) / 2) * 255).astype(np.uint8))
 
 
-def for_one_image(facepp_client, image_url, model_path, age):
-    model = load_model(model_path)
+def for_one_image(facepp_client, image_url, model, age):
     target_filepath = "metric_images/result.jpg"
     generate_image(model, image_url, age, target_filepath)
 
     cmp = facepp_client.compare.get(image_file1=image_url,
                                     image_file2=target_filepath)
 
-    print('thresholds', '=', json.dumps(cmp.thresholds, indent=4))
-    print('confidence', '=', cmp.confidence)
+    try:
+        return cmp.confidence, cmp.thresholds["1e-5"]
+    except ResourceAttrError:
+        return np.nan, np.nan
 
-    return cmp.confidence, json.dumps(cmp.thresholds)
+
+def for_more_images(facepp_client, dataset_url, model_path, age, count):
+    image_paths = list_full_paths(dataset_url)
+    np.random.shuffle(image_paths)
+    image_paths = image_paths[:count]
+
+    model = load_model(model_path)
+
+    confidences = []
+    thresholds = []
+    for image in image_paths:
+        confidence, threshold = for_one_image(facepp_client, image, model, age)
+
+        confidences.append(confidence)
+        thresholds.append(threshold)
+
+    return np.nanmean(np.asarray(confidences)), np.nanmean(np.asarray(thresholds))
 
 
 if __name__ == "__main__":
     facepp_client = create_facepp_client('face++_config.ini')
     model_paths = read_caae_paths('face++_config.ini')
 
-    for_one_image(facepp_client, 'metric_images/test.jpg', model_paths['patchgan'], 40)
+    # conf, thresholds = for_one_image(facepp_client, 'metric_images/test.jpg', model_paths['patchgan'], 40)
+    # print(conf)
+    # print(thresholds["1e-5"])
+
+    print(for_more_images(facepp_client, '../../data/UTKFace', model_paths['patchgan'], 40, 1000))
+
