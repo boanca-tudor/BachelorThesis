1f1cbb40b98f76c62dae447ce1f3f3b192786e82
boanca-tudor <tudorboanca@gmail.com>
2023-06-13 10:32:30 +0300

    Metrics code changed and added aging accuracy

---
+++ Project/models/metrics/aging_accuracy.py
@@ -0,0 +1,87 @@
+import time
+
+import tensorflow as tf
+from facepplib.exceptions import ResourceAttrError, ResourceSetIndexError
+from skimage.io import imsave
+import numpy as np
+
+from facepp_utils import *
+from models.caae import CAAE
+from models.model_utils import *
+
+
+def load_model(model_path):
+    model = CAAE(z_channels=100,
+                 l_channels=10,
+                 gen_channels=1024)
+
+    model.load_model(model_path)
+
+    return model
+
+
+def generate_image(model, base_image_url, age, target_filepath):
+    image = load_image(base_image_url)
+    image = tf.expand_dims(image, axis=0)
+    result = tf.squeeze(model.eval([image, age])).numpy()
+
+    imsave(target_filepath, (((result + 1) / 2) * 255).astype(np.uint8))
+
+
+def for_one_image(facepp_client, image_url, model, age):
+    target_filepath = "metric_images/result.jpg"
+    generate_image(model, image_url, age, target_filepath)
+
+    cmp = facepp_client.image.get(image_file=target_filepath)
+
+    try:
+        return cmp.faces[0].age['value']
+    except ResourceAttrError:
+        return np.nan
+    except ResourceSetIndexError:
+        return np.nan
+
+
+def for_more_images(facepp_client, images, model_path, age):
+    model = load_model(model_path)
+
+    ages = []
+    for image in images:
+        ages.append(for_one_image(facepp_client, image, model, age))
+        time.sleep(.5)
+
+    return np.nanmean(ages)
+
+
+if __name__ == "__main__":
+    facepp_client = create_facepp_client('face++_config.ini')
+    model_paths = read_caae_paths('face++_config.ini')
+
+    # images21_30 = np.load('21-30.npy')
+    # images31_40 = np.load('31-40.npy')
+    images41_50 = np.load('41-50.npy')
+    # images51 = np.load('51+.npy')
+    #
+    # images51_ages = []
+    # for image in images51:
+    #     images51_ages.append(for_one_image(facepp_client, image, None, None)['value'])
+    #     time.sleep(.5)
+    #
+    # print(np.nanmean(images51_ages))
+
+    age = 55
+    model_path = '../../' + model_paths['base']
+    images = images41_50
+    print(for_more_images(facepp_client, images, model_path, age))
+
+    # image_paths = list_full_paths(dataset_url)
+    # np.random.shuffle(image_paths)
+    # image_paths = image_paths[:count]
+    #
+    # ages = create_all_ages()
+    # results = []
+    # for age in ages:
+    #     results.append(for_more_images(facepp_client, image_paths, '../../' + model_paths['wgan'], age))
+    # print(ages)
+    # print(results)
+
--- Project/models/metrics/inception_score.py
+++ Project/models/metrics/inception_score.py
@@ -1,12 +1,18 @@
 from math import floor
+
+import numpy as np
 from numpy import expand_dims
 from numpy import log
 from numpy import mean
 from numpy import std
 from numpy import exp
-from keras.applications.inception_v3 import InceptionV3
-from keras.applications.inception_v3 import preprocess_input
-from skimage.transform import resize
+from keras_vggface.utils import preprocess_input
+
+from models.caae import CAAE
+from models.age_classifier import *
+from skimage.io import imsave
+
+from models.metrics.facepp_utils import read_caae_paths
 
 
 def scale_images(images, new_shape):
@@ -15,22 +21,21 @@ def scale_images(images, new_shape):
         new_image = resize(image, new_shape, 0)
         images_list.append(new_image)
 
-    return images_list
+    return np.asarray(images_list)
 
 
 def calculate_inception_score(images, n_split=10, eps=1E-16):
-    model = InceptionV3()
+    inception_model = create_model(128)
+    inception_model.load_weights("../../age_classifier_checkpoints/weights.hdf5")
     scores = list()
     n_part = floor(images.shape[0] / n_split)
     for i in range(n_split):
         ix_start, ix_end = i * n_part, (i + 1) * n_part
         subset = images[ix_start: ix_end]
 
-        subset = subset.astype('float32')
-        subset = scale_images(subset, (299, 299, 3))
         subset = preprocess_input(subset)
 
-        p_yx = model.predict(subset)
+        p_yx = inception_model.predict(subset)
 
         p_y = expand_dims(p_yx.mean(axis=0), 0)
         kl_d = p_yx * (log(p_yx + eps) - log(p_y + eps))
@@ -41,3 +46,35 @@ def calculate_inception_score(images, n_split=10, eps=1E-16):
 
     return mean(scores), std(scores)
 
+
+if __name__ == "__main__":
+    dataset_path = "../../data/UTKFace"
+    model = CAAE(z_channels=100,
+                 l_channels=10,
+                 gen_channels=1024,
+                 dataset_size=len(list_full_paths(dataset_path)))
+
+    checkpoints = read_caae_paths('face++_config.ini')
+    checkpoint_dir = '../../' + checkpoints['patchwgan']
+
+    model.load_model(checkpoint_dir)
+
+    ages = create_all_ages()
+    image_paths = list_full_paths(dataset_path)
+    np.random.shuffle(image_paths)
+
+    image_paths = image_paths[:1000]
+
+    images = []
+    for image in image_paths:
+        images.append(tf.expand_dims(load_image(image), axis=0))
+    results = []
+
+    for image in images:
+        for age in ages:
+            res = tf.squeeze(model.eval([image, age])).numpy()
+            res = (((res + 1) / 2) * 255).astype(np.uint8)
+            results.append(res.astype('float64'))
+
+    is_avg, is_std = calculate_inception_score(np.asarray(results), n_split=12)
+    print(is_avg, is_std)
